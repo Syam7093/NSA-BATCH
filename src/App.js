e0275e649f333f247a76185eb88f507c66a943d6
syamsaiyek <msyamsundarsai@gmail.com>
2023-04-19 18:44:35 +0530

    adding all the files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Syam from './components/Syam';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>this is syam</h1>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/Syam.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Syam() {
+  return (
+    <div>
+        <h1>this is syam profile</h1>
+    </div>
+  )
+}
+
+export default Syam
