5417206d5c00f3918a3515fdd903b62dec6dfcde
syamsaiyek <msyamsundarsai@gmail.com>
2023-04-20 09:58:26 +0530

    addillng

--- src/App.js
+++ src/App.js
@@ -5,23 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>this is syam</h1>
-     <h1>ramehsh</h1>
-    <h1>i have done some changes....</h1>
-    <h1>nazeema done some changees</h1>
-
-    <h1>anil kumar</h1>
-    <h1>anil </h1>
-    <h3>sam</h3>
-    <h2>symsai</h2>
-    <h1>manju</h1>
-    <h1>gopi</h1>
-    <h1>lalallagit</h1>
-    <h1>hahahaha</h1>
-    <h1>ravi</h1>
-    <h1>jbvbvb</h1>
-    <h2>BRN infotech</h2>
-    <h2>INDIA</h2>
+    <h1>Welcome to new project</h1>
       </div>
   );
 }
