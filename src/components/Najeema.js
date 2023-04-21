7bfd1bfbae859f6d7c868e72726e3b2b1a9374a9
srujan <johndoe@example.com>
2023-04-21 12:47:44 +0530

    anil

--- src/components/Najeema.js
+++ src/components/Najeema.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap'
 
+
+
 function Najeema() {
+  
   return (
-    <div>
-      <Carousel>
-        <Carousel.Item>
+   <div >
+   <div>
+      <Carousel >
+        <Carousel.Item >
           <img
             className="d-block w-100"
-            src="https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/a37c7aa9669fcd4c.jpg?q=50"
+            src="https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/a37c7aa9669fcd4c.jpg?q=50,"
             alt="Third slide"
           />
         </Carousel.Item>
@@ -48,7 +52,7 @@ function Najeema() {
           />
         </Carousel.Item>
       </Carousel>
-      
+      </div>
       <div className="mainDiv">
         <div className="allInOne " >
           <img src="https://rukminim1.flixcart.com/image/416/416/l5fnhjk0/dslr-camera/f/t/m/eos-r10-24-2-r10-canon-original-imagg42fsbgv79da.jpeg?q=70"alt="hi" height="200px" width="200px;"></img>
@@ -87,6 +91,7 @@ function Najeema() {
         <h6>Upto 80% Off</h6>
         <h6>Big Discounts</h6>
         </div>
+       
       </div>
 
 
